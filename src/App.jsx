a9065be7a656cdf196c9943c93e9df8a97514d5a
asitkr <krsinhasaurabh@gmail.com>
2025-10-26 12:59:26 +0530

    first commit done

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import Art from './components/Art';
+import Hero from './components/Hero';
+import Menu from './components/Menu';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Cocktails from './components/Cocktails';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+    </main>
+  )
+}
+
+export default App;
